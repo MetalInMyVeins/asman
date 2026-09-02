a89326d826358d2ec650d83a02bdc64e5b7774bf
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2026-09-03 04:27:23 +0600

    p1f: init

--- src/freest/x86_64/p1.asm
+++ src/freest/x86_64/p1.asm
@@ -1,7 +1,18 @@
+section .rodata
+	txt db "Hello world", 10, 0
+	txt_len equ $ - txt
+
+section .data
+
 section .text
 	global _start
 
 _start:
+	mov	rax, 1
+	mov	edi, 1
+	lea	rsi, [rel txt]
+	mov	edx, txt_len
+	syscall
 	mov	rax, 60
 	mov	edi, 0
 	syscall
