79669273ec01ea872cfd08e45388953c12894a93
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2026-09-03 03:59:02 +0600

    p1h: init

--- src/hosted/x86_64/p1.asm
+++ src/hosted/x86_64/p1.asm
@@ -1,10 +1,43 @@
+section .rodata
+	; string literals go in the read-only data section.
+	; 10 is the newline character in ascii while 0 is the NUL character.
+	txt db "Hello world", 10, 0
+
+section .data
+
 section .text
+	; to make the main symbol visible to linker.
 	global main
+	; for using printf from libc.
+	extern printf
 
 main:
+	; function prologue.
+	; basically it first pushes the base pointer on the stack to save its previous value.
+	; where did the previous value come from? from _start, which is defined in the runtime provided by libc.
+	; it is _start which calls main.
+	; then the value of stack pointer is stored in rbp register.
+	; so from that point on, rbp acts as the base of the current stack frame.
+	; rsp denotes the top of the stack.
 	push	rbp
 	mov	rbp, rsp
+	; rsp is decremented by 16 bytes to conform to the SysV ABI requirement of stack frame
+	; being 16 bytes aligned before any call instruction.
+	; 128 bytes below rsp is called red zone which can be used as temporary storage.
+	; but they're guaranteed to be clobbered if a call instruction executes before moving rsp down.
+	sub	rsp, 16
 
+	; SysV calling convention.
+	lea	rdi, [rel txt]
 	xor	eax, eax
+	call	printf
+
+	; setting all 32 bits of eax to zero which zero extends and makes the entire rax 0.
+	xor	eax, eax
+	; it does two things.
+	; first it moves the value of rbp to rsp.
+	; then it pops the saved rbp from stack and stores in itself.
+	; so basically after this instruction, rsp and rbp both are back where they started.
 	leave
+	; resumes execution to the return address of caller.
 	ret
